254e545f8dbada479328e69bd0b94eee4a06d5e5
Brito Maria Luz <britomluz@gmail.com>
2021-12-30 18:22:30 -0300

    audio player spanish fixed

--- scripts/typing.js
+++ scripts/typing.js
@@ -68,15 +68,18 @@ const songs = ['crazy', 'knockin', 'supergirl', 'undercover', 'wonderwall'];
 
 // Keep track of song
 let songIndex = 2;
-
+let ruta1 = './'
+let ruta2 = '../'
 // Initially load song details into DOM
-loadSong(songs[songIndex]);
+loadSong(ruta1, songs[songIndex]);
+loadSong(ruta2, songs[songIndex]);
 
 // Update song details
-function loadSong(song) {
+function loadSong(ruta, song) {
   title.innerText = song;
-  audio.src = `/music/${song}.mp3`;
-  cover.src = `/assets/${song}.jpg`;
+  audio.src = `${ruta}music/${song}.mp3`;
+  cover.src = `${ruta}assets/${song}.jpg`;  
+   
 }
 
 // Play song
@@ -86,6 +89,7 @@ function playSong() {
   playBtn.querySelector('i.bi').classList.add('bi-pause-circle-fill');
 
   audio.play();
+ 
 }
 
 // Pause song
@@ -95,6 +99,7 @@ function pauseSong() {
   playBtn.querySelector('i.bi').classList.remove('bi-pause-circle-fill');
 
   audio.pause();
+ 
 }
 
 // Previous song
@@ -139,68 +144,6 @@ function setProgress(e) {
   audio.currentTime = (clickX / width) * duration;
 }
 
-//get duration & currentTime for Time of song
-function DurTime (e) {
-	const {duration,currentTime} = e.srcElement;
-	var sec;
-	var sec_d;
-
-	// define minutes currentTime
-	let min = (currentTime==null)? 0:
-	 Math.floor(currentTime/60);
-	 min = min <10 ? '0'+min:min;
-
-	// define seconds currentTime
-	function get_sec (x) {
-		if(Math.floor(x) >= 60){
-			
-			for (var i = 1; i<=60; i++){
-				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
-					sec = Math.floor(x) - (60*i);
-					sec = sec <10 ? '0'+sec:sec;
-				}
-			}
-		}else{
-		 	sec = Math.floor(x);
-		 	sec = sec <10 ? '0'+sec:sec;
-		 }
-	} 
-
-	get_sec (currentTime,sec);
-
-	// change currentTime DOM
-	currTime.innerHTML = min +':'+ sec;
-
-	// define minutes duration
-	let min_d = (isNaN(duration) === true)? '0':
-		Math.floor(duration/60);
-	 min_d = min_d <10 ? '0'+min_d:min_d;
-
-
-	 function get_sec_d (x) {
-		if(Math.floor(x) >= 60){
-			
-			for (var i = 1; i<=60; i++){
-				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
-					sec_d = Math.floor(x) - (60*i);
-					sec_d = sec_d <10 ? '0'+sec_d:sec_d;
-				}
-			}
-		}else{
-		 	sec_d = (isNaN(duration) === true)? '0':
-		 	Math.floor(x);
-		 	sec_d = sec_d <10 ? '0'+sec_d:sec_d;
-		 }
-	} 
-
-	// define seconds duration
-	
-	get_sec_d (duration);
-
-	// change duration DOM
-	durTime.innerHTML = min_d +':'+ sec_d;
-		
-};
 
 // Event listeners
 playBtn.addEventListener('click', () => {
@@ -225,9 +168,6 @@ progressContainer.addEventListener('click', setProgress);
 
 // Song ends
 audio.addEventListener('ended', nextSong);
-
-// Time of song
-audio.addEventListener('timeupdate',DurTime);
 
 
 // TYPING ANIMATION ENGLISH
@@ -289,26 +229,26 @@ let charIndex2 = 0;
 
 function type2() {
   if (charIndex2 < textArray2[textArray2Index].length) {
-    if(!cursorSpan2.classList.contains("typing-es")) cursorSpan2.classList.add("typing-es");
+    if(!cursorSpan2.classList.contains("typing")) cursorSpan2.classList.add("typing");
     typedTextSpan2.textContent += textArray2[textArray2Index].charAt(charIndex2);
     charIndex2++;
     setTimeout(type2, typingDelay2);
   } 
   else {
-    cursorSpan2.classList.remove("typing-es");
+    cursorSpan2.classList.remove("typing");
   	setTimeout(erase2, newTextDelay2);
   }
 }
 
 function erase2() {
 	if (charIndex2 > 0) {
-    if(!cursorSpan2.classList.contains("typing-es")) cursorSpan2.classList.add("typing-es");
+    if(!cursorSpan2.classList.contains("typing")) cursorSpan2.classList.add("typing");
     typedTextSpan2.textContent = textArray2[textArray2Index].substring(0, charIndex2-1);
     charIndex2--;
     setTimeout(erase2, erasingDelay2);
   } 
   else {
-    cursorSpan2.classList.remove("typing-es");
+    cursorSpan2.classList.remove("typing");
     textArray2Index++;
     if(textArray2Index>=textArray2.length) textArray2Index=0;
     setTimeout(type2, typingDelay2 + 1100);
